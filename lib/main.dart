b43088afb780cbfb5445a2ecfe2a37c92e46aa9f
raj vaya <rajkumarvaya@gmail.com>
2019-09-07 16:35:57 +0530

    Working code with new UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/DiceRoll.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+void main() => runApp(myapp());
+
+
+class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey
-      ),
-      home: Roll(),
-    );
+   return MaterialApp(
+     title: 'flutter demo',
+     home: Roll(),
+     theme: ThemeData(
+       primaryColor: Colors.blueGrey
+     ),
+   );
   }
 }
-
